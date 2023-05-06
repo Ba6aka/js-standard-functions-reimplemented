57eb817e419c760911e2e92824e2205ff9953bab
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-06 12:32:47 +0300

    create folders and files for array methods

--- helpers/get-all-array-methods.js
+++ helpers/get-all-array-methods.js
@@ -1,14 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
 function getAllArrayMethods() {
   let methodsString = ''
   const methods = Object.getOwnPropertyNames(Array.prototype).filter(function (prop) {
     return typeof Array.prototype[prop] === 'function';
-  }).map(function (prop) {
-    return prop + '()';
-  });
+  })
 
   methods.forEach((method) => {
     methodsString += `- .${method}\n`
   })
 
-  return methodsString
+  return methods
 }
+
+
+
+function createFoldersAndFiles(folderNames) {
+  folderNames.forEach(folderName => {
+    const folderPath = `array/instance/${folderName}`;
+
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+    }
+
+    fs.writeFileSync(path.join(folderPath, `${folderName}.js`), '');
+  });
+}
+
+
+createFoldersAndFiles(getAllArrayMethods());
