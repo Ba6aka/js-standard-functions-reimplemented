73ca319fadc581ee8892c9ba15413bcce558fd75
UniBreakfast <unibreakfast1@gmail.com>
2023-05-10 21:12:20 +0300

    update at method

--- array/instance/at/at.js
+++ array/instance/at/at.js
@@ -21,3 +21,9 @@ Array.prototype.at = function at(index) {
 
   return result
 }
+
+Array.prototype.at = function at(index) {
+  index |= 0
+  
+  return index < 0 ? this[this.length + index] : this[index]
+}
