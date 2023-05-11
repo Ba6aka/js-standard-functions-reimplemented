9ca8fcb85e614d4f5122705ec6e48005de92082c
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-11 18:33:12 +0300

    refactor italics and test-italics

--- string/instance/italics/italics.js
+++ string/instance/italics/italics.js
@@ -1,24 +1,3 @@
-String.prototype.italics = function italics() {
-  let result = ""
-  let openTag = "<i>"
-  let closeTag = "</i>"
-  let inTag = false
-
-  for (let i = 0; i < this.length; i++) {
-    const char = this[i]
-
-    if (char === "<") {
-      result += openTag
-      inTag = true
-    } else if (char === ">") {
-      result += closeTag
-      inTag = false
-    } else if (!inTag) {
-      result += openTag + char + closeTag
-    } else {
-      result += char
-    }
-  }
-
-  return result
+String.prototype.italics = function() {
+  return '<i>' + this + '</i>'
 }
--- string/instance/italics/test-italics.js
+++ string/instance/italics/test-italics.js
@@ -1,37 +1,38 @@
-function testItalics() {
-  {// Test case 1
-    const str1 = "This is a test"
-    const str2 = str1.italics()
+require('./italics.js')
 
-    if (str2 !== '<i>This is a test</i>') {
+function testItalics() {
+  {// Test case 1: standard
+    const str = "This is a test"
+    const strItalics = str.italics()
+
+    if (strItalics !== '<i>This is a test</i>') {
       console.error('Test case 1 failed')
     } else {
       console.log('Test case 1 passed')
     }
   }
 
-  {// Test case 2
-    const str3 = "<b>This is bold text</b>"
-    const str4 = str3.italics()
+  {// Test case 2: string with other tag
+    const str = "<b>This is bold text</b>"
+    const strItalics = str.italics()
 
-    if (str4 !== '<i><b>This is bold text</b></i>') {
+    if (strItalics !== '<i><b>This is bold text</b></i>') {
       console.error('Test case 2 failed')
     } else {
       console.log('Test case 2 passed')
     }
   }
 
-  {// Test case 3
-    const str5 = "This is <b>bold text</b> and this is not"
-    const str6 = str5.italics()
+  {// Test case 3: empty string
+    const str = ""
+    const strItalics = str.italics()
 
-    if (str6 !== '<i>This is <b>bold text</b> and this is not</i>') {
+    if (strItalics !== '<i></i>') {
       console.error('Test case 3 failed')
     } else {
       console.log('Test case 3 passed')
     }
   }
-  
 }
 
 testItalics()
