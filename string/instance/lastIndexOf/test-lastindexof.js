d42673fd3544b3a03d8e92f76f9378e3f40675e0
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-11 18:35:44 +0300

    refactor test-lastindexof

--- string/instance/lastIndexOf/test-lastindexof.js
+++ string/instance/lastIndexOf/test-lastindexof.js
@@ -1,46 +1,38 @@
 require('./lastIndexOf.js')
 
 function testLastIndexOf() {
-  {// Test case 1
-    const str1 = "Hello, world!"
-    const index1 = str1.lastIndexOf("o")
-    if (index1 !== 8) {
+  {// Test case 1: standard
+    const str = "Hello, world!"
+    const index = str.lastIndexOf("o")
+
+    if (index !== 8) {
       console.error('Test case 1 failed')
     } else {
       console.log('Test case 1 passed')
     }
   }
 
-  {// Test case 2
-    const str2 = "Hello, world!"
-    const index2 = str2.lastIndexOf("o", 5)
-    if (index2 !== 4) {
+  {// Test case 2: empty string
+    const str = ""
+    const index = str.lastIndexOf("o", 5)
+
+    if (index !== -1) {
       console.error('Test case 2 failed')
     } else {
       console.log('Test case 2 passed')
     }
   }
 
-  {// Test case 3
-    const str3 = "Hello, world!"
-    const index3 = str3.lastIndexOf("z")
-    if (index3 !== -1) {
+  {// Test case 3: wrong argument
+    const str = "Hello, world!"
+    const index = str.lastIndexOf("z")
+    
+    if (index !== -1) {
       console.error('Test case 3 failed')
     } else {
       console.log('Test case 3 passed')
     }
   }
-
-  {// Test case 4
-    const str4 = "Hello, world!"
-    const index4 = str4.lastIndexOf("H")
-    if (index4 !== 0) {
-      console.error('Test case 4 failed')
-    } else {
-      console.log('Test case 4 passed')
-    }
-  }
-
 }
 
 testLastIndexOf()
