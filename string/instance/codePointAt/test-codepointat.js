84bfc636c3c048dcc9003d0b211a8163354cc056
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-11 17:54:42 +0300

    refactor test-codepointat

--- string/instance/codePointAt/test-codepointat.js
+++ string/instance/codePointAt/test-codepointat.js
@@ -1,28 +1,27 @@
 require('./codePointAt.js')
 
 function testCodePointAt() {
-  {// Test case 1
-    const str1 = 'Hello, world!'
-    const code1 = str1.codePointAt(1)
+  {// Test case 1: standard
+    const str = 'Hello, world!'
+    const code = str.codePointAt(1)
 
-    if (code1 !== 101) {
+    if (code !== 101) {
       console.error('Test case 1 failed')
     } else {
       console.log('Test case 1 passed')
     }
   }
 
-  {// Test case 2
-    const str2 = '\uD83D\uDCA9\uD83D\uDC51'
-    const code2 = str2.codePointAt(0)
+  {// Test case 2: Unicode 
+    const str = '\uD83D\uDCA9\uD83D\uDC51'
+    const code = str.codePointAt(0)
 
-    if (code2 !== 0x1F4A9) {
+    if (code !== 0x1F4A9) {
       console.error('Test case 2 failed')
     } else {
       console.log('Test case 2 passed')
     }
   }
-
 }
 
 testCodePointAt()
