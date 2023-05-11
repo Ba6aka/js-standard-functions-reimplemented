df037cf60f7f0d5716d8c15769808ad27f61bba1
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-11 18:16:59 +0300

    refactor test-fontcolor

--- string/instance/fontcolor/test-fontcolor.js
+++ string/instance/fontcolor/test-fontcolor.js
@@ -2,10 +2,10 @@ require('./fontcolor.js')
 
 function testFontcolor() {
   {// Test case 1: standard
-    const str1 = "Hello, world!"
-    const coloredStr1 = str1.fontcolor("red")
+    const str = "Hello, world!"
+    const coloredStr = str.fontcolor("red")
 
-    if (coloredStr1 !== '<font color="red">Hello, world!</font>') {
+    if (coloredStr !== '<font color="red">Hello, world!</font>') {
       console.error('Test case 1 failed')
     } else {
       console.log('Test case 1 passed')
@@ -13,10 +13,10 @@ function testFontcolor() {
   }
 
   {// Test case 2: empry string
-    const str2 = ""
-    const coloredStr2 = str2.fontcolor("blue")
+    const str = ""
+    const coloredStr = str.fontcolor("blue")
 
-    if (coloredStr2 !== '<font color="blue"></font>') {
+    if (coloredStr !== '<font color="blue"></font>') {
       console.error('Test case 2 failed')
     } else {
       console.log('Test case 2 passed')
