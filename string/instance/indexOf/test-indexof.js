0bc86d64f1d9c2bcf872e4d99f970d7722874179
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-11 18:27:51 +0300

    refactor test-indexof

--- string/instance/indexOf/test-indexof.js
+++ string/instance/indexOf/test-indexof.js
@@ -1,33 +1,33 @@
 require('./indexOf.js')
 
 function testIndexOf() {
-  {// Test case 1
-    const str1 = "Hello, world!"
-    const index1 = str1.indexOf("world")
+  {// Test case 1: standard
+    const str = "Hello, world!"
+    const index = str.indexOf("world")
 
-    if (index1 !== 7) {
+    if (index !== 7) {
       console.error('Test case 1 failed')
     } else {
       console.log('Test case 1 passed')
     }
   }
 
-  {// Test case 2
-    const str2 = "Hello, world!"
-    const index2 = str2.indexOf("world", 8)
+  {// Test case 2: with wrong start position
+    const str = "Hello, world!"
+    const index = str.indexOf("world", 8)
 
-    if (index2 !== -1) {
+    if (index !== -1) {
       console.error('Test case 2 failed')
     } else {
       console.log('Test case 2 passed')
     }
   }
 
-  {// Test case 3
-    const str3 = "Hello, world!"
-    const index3 = str3.indexOf("foo")
+  {// Test case 3: with wrong argument
+    const str = "Hello, world!"
+    const index = str.indexOf("foo")
 
-    if (index3 !== -1) {
+    if (index !== -1) {
       console.error('Test case 3 failed')
     } else {
       console.log('Test case 3 passed')
