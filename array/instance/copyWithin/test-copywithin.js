2da73dfe2b7529295e685eb031b24300cf8e0ca1
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-15 20:33:52 +0300

    refactor test-copywithin

--- array/instance/copyWithin/test-copywithin.js
+++ array/instance/copyWithin/test-copywithin.js
@@ -1,46 +1,57 @@
 require('./copyWithin.js')
+
 function testCopyWithin() {
+  const { stringify } = JSON
 
-  { // Test case 1
-    const arr1 = [1, 2, 3, 4, 5]
-    let result = arr1.copyWithin(-1)
-    if (result.toString() !== [1, 2, 3, 4, 1].toString()) {
-      console.error('Test case 1 failed')
+  { // Test case 1: negative argument
+    const arr = [1, 2, 3, 4, 5]
+    const expected = [1, 2, 3, 4, 1]
+    const actual = arr.copyWithin(-1)
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 1, negative argument: failed, expected ${stringify(expected)}, but got ${stringify(actual)}`)
     } else {
-      console.log('Test case 1: passed')
+      console.log('Test 1, negative argument: passed')
     }
   }
 
-  { // Test case 2
-    const arr2 = [1, 2, 3, 4, 5]
-    let result = arr2.copyWithin(0, 3)
-    if (result.toString() !== [4, 5, 3, 4, 5].toString()) {
-      console.error('Test case 2 failed')
-    } else {
-      console.log('Test case 2: passed')
+  { // Test case 2: two argument
+    const arr = [1, 2, 3, 4, 5]
+    const expected = [4, 5, 3, 4, 5]
+    const actual = arr.copyWithin(0, 3)
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 2, two argument: failed, expected ${stringify(expected)}, but got ${stringify(actual)}`)
+    }
+    else {
+      console.log('Test 2, two argument: passed')
     }
   }
 
-  { // Test case 3
-    const arr3 = [1, 2, 3, 4, 5]
-    let result = arr3.copyWithin(1, 3, 4)
-    if (result.toString() !== [1, 4, 3, 4, 5].toString()) {
-      console.error('Test case 3 failed')
+  { // Test case 3: three argument
+    const arr = [1, 2, 3, 4, 5]
+    const expected = [1, 4, 3, 4, 5]
+    const actual = arr.copyWithin(1, 3, 4)
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.log(`Test 3, three argument: failed, expected ${stringify(expected)}, but got ${stringify(actual)}`)
     } else {
-      console.log('Test case 3: passed')
+      console.log('Test 3, three argument: passed')
     }
   }
 
-  { // Test case 4
-    const arr5 = [1, 2, 3, 4, 5]
-    let result = arr5.copyWithin(2, 0, 2)
-    if (result.toString() !== [1, 2, 1, 2, 5].toString()) {
-      console.error('Test case 4 failed')
-    } else {
-      console.log('Test case 4: passed')
+  { // Test case 4:  three argument with 0
+    const arr = [1, 2, 3, 4, 5]
+    const expected = [1, 2, 1, 2, 5]
+    const actual = arr.copyWithin(2, 0, 2)
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 4, three arrays: failed, expected ${stringify(expected)}, but got ${stringify(actual)}`)
+    }
+    else {
+      console.log('Test 4, three argument with 0: passed')
     }
   }
-
 }
 
 testCopyWithin()
