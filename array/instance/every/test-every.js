c5454aa4ad4bc0f4c385ec669edb5041aa8422dd
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-15 21:03:37 +0300

    refactor test-every

--- array/instance/every/test-every.js
+++ array/instance/every/test-every.js
@@ -1,56 +1,60 @@
 require('./every.js')
 
 function testEvery() {
-  { // Test case 1
+  const { stringify } = JSON
+
+  { // Test case 1: even case
     const arr = [2, 4, 6, 8, 10]
+    const expected = true
+    const actual = arr.every((num) => num % 2 === 0)
 
-    let areAllEven = arr.every((num) => num % 2 === 0)
-
-    if (!areAllEven) {
-      console.error('Test case 1 failed')
+    if (!actual) {
+      console.error(`Test 2, even case: failed, expected: ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 1: passed')
+      console.log(`Test 2, even case: passed,`)
     }
   }
 
-  { // Test case 2
+  { // Test case 2: odd case
     const arr = [1, 3, 5, 7, 9]
+    const expected = true
+    const actual = arr.every((num) => num % 2 !== 0)
 
-    let areAllOdd = arr.every((num) => num % 2 !== 0)
-
-    if (!areAllOdd) {
-      console.error('Test case 2 failed')
+    if (actual !== expected) {
+      console.error(`Test 2, odd case: failed, expected: ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 2: passed')
+      console.log('Test , odd case: passed')
     }
   }
 
-  { // Test case 3
+  { // Test case 3: even reverse case
     const arr = [2, 4, 5, 6, 8]
+    const expected = false
+    const actual = arr.every((num) => num % 2 === 0)
 
-    let areAllEven = arr.every((num) => num % 2 === 0)
-
-    if (areAllEven) {
-      console.error('Test case 3 failed')
+    if (!expected === actual) {
+      console.error(`Test 3, even reverse case: failed, expected: ${!expected}, but got ${actual}`)
     } else {
-      console.log('Test case 3: passed')
+      console.log('Test 3, even reverse case: passed')
     }
   }
 
   { // Test case 4: wrong argument
     const arr = [1, 2, 3, 4, 5]
-    
+    const expected = undefined
+
+    let actual
+
     try {
-      arr.every()
+      actual = arr.every()
     } catch (err) {
-      if (err.message !== 'undefined is not a function') {
-        console.error('Test case 4 failed')
+      if (stringify(actual) !== stringify(expected)) {
+        console.error(`Test 4, wrong argumet: failed, expected: ${expected}, but got ${actual}`)
       } else {
-        console.log('Test case 4: passed')
+        console.log('Test 4, wrong argument: passed')
       }
     }
   }
-
- }
+}
 
 testEvery()
