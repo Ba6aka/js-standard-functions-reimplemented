0d257e773d464e2649e3f3f4af3b4810640b9f65
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-16 21:55:43 +0300

    refacrot test-find

--- array/instance/find/test-find.js
+++ array/instance/find/test-find.js
@@ -1,51 +1,63 @@
 require('./find.js')
 
 function testFind() {
-  { // Test case 1
-    const arr1 = [1, 2, 3, 4, 5]
-    const found = arr1.find((num) => num === 3)
-    if (found !== 3) {
-      console.error('Test case 1 failed')
+  const { stringify } = JSON
+
+  { // Test case 1: find number
+    const arr = [1, 2, 3, 4, 5]
+    const expected = 3
+    const actual = arr.find((num) => num === 3)
+
+    if (stringify(expected) !== stringify(actual)) {
+      console.error(`Test 1, find number: failed, expected ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 1: passed')
+      console.log('Test 1, find number: passed')
     }
   }
 
-  { // Test case 2 
-    const arr2 = ['apple', 'banana', 'orange']
-    const found = arr2.find((fruit) => fruit.startsWith('b'))
-    if (found !== 'banana') {
-      console.error('Test case 2 failed')
+  { // Test case 2 : find letter
+    const arr = ['apple', 'banana', 'orange']
+    const expected = 'banana'
+    const actual = arr.find((fruit) => fruit.startsWith('b'))
+
+    if (stringify(expected) !== stringify(actual)) {
+      console.error(`Test 2, find letter: failed, expected ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 2: passed')
+      console.log('Test 2, find letter: passed')
     }
   }
 
   { //Test case 3: none passing
-    const arr3 = [1, 2, 3, 4, 5]
-    const found = arr3.find((num) => num > 10)
-    if (found !== undefined) {
-      console.error('Test case 3 failed')
+    const arr = [1, 2, 3, 4, 5]
+    const expected = undefined
+    const actual = arr.find((num) => num > 10)
+
+    if (stringify(expected) !== stringify(actual)) {
+      console.error(`Test 3, none passing: failed, expected ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 3: passed')
+      console.log('Test 3, none passing: passed')
     }
   }
 
   { //Test case 4: wrong argument
-    const arr4 = [1, 2, 3, 4, 5]
+    const arr = [1, 2, 3, 4, 5]
+    const expected = undefined
+
+    let actual
     try {
-      arr4.find()
+      actual = arr.find()
     } catch (err) {
-      if (err.message !== 'undefined is not a function') {
-        console.error('Test case 4 failed')
+      if (stringify(actual) !== stringify(expected)) {
+        console.error(`Test 4, wrong argument: failed, expected ${expected}, bu got ${actual}`)
       } else {
-        console.log('Test case 4: passed')
+        console.log('Test 4, wrong argument: passed')
       }
     }
   }
 
   { //Test case 5: thisArg
     const arr = [1, 2, 3, 4, 5]
+    const expected = 3
     const expert = {
       values: [3, 4, 5],
       check(item) {
@@ -53,11 +65,11 @@ function testFind() {
       }
     }
 
-    const found = arr.find(expert.check, expert)
-    if (found !== 3) {
-      console.error('Test case 5 failed')
+    const actual = arr.find(expert.check, expert)
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 5, thisArg: failed, expected ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 5: passed')
+      console.log('Test 5, thisArg: passed')
     }
   }
 }
