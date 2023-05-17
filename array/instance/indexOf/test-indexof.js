a3a1bf721f95cfa851eab51604a2790da0e3ad26
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-17 21:50:29 +0300

    refactor test-indexof

--- array/instance/indexOf/test-indexof.js
+++ array/instance/indexOf/test-indexof.js
@@ -1,52 +1,66 @@
 require('./indexOf.js')
 
+
 function testIndexOf() {
-  { // Test case 1
-    const arr1 = [1, 2, 3, 4, 5]
+  const { stringify } = JSON
 
-    if (arr1.indexOf(3) !== 2) {
-      console.error('Test case 1 failed')
+  { // Test case 1: normal case
+    const arr = [1, 2, 3, 4, 5]
+    const expected = 2
+    const actual = arr.indexOf(3)
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 1, normal case: failed, expected: ${expected}, but got actual: ${actual}`)
     } else {
-      console.log('Test case 1: passed')
+      console.log('Test 1, normal case: passed')
     }
   }
 
-  {  // Test case 2
-    const arr2 = [1, 2, 3, 4, 5]
+  {  // Test case 2: out range argument
+    const arr = [1, 2, 3, 4, 5]
+    const expected = -1
+    const actual = arr.indexOf(6)
 
-    if (arr2.indexOf(6) !== -1) {
-      console.error('Test case 2 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 2, out range argument: failed, expected: ${expected}, but got actual: ${actual}`)
     } else {
-      console.log('Test case 2: passed')
-    }
-  }
-  {  // Test case 3
-    const arr3 = ['apple', 'banana', 'orange']
-
-    if (arr3.indexOf('banana') !== 1) {
-      console.error('Test case 3 failed')
-    } else {
-      console.log('Test case 3: passed')
+      console.log('Test 2, out range argument: passed')
     }
   }
 
-  { // Test case 4
-    const arr4 = ['apple', 'banana', 'orange']
+  {  // Test case 3: array of string
+    const arr = ['apple', 'banana', 'orange']
+    const expected = 1
+    const actual = arr.indexOf('banana')
 
-    if (arr4.indexOf('grape') !== -1) {
-      console.error('Test case 4 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 3, array of string: failed, expected: ${expected}, but got actual: ${actual}`)
     } else {
-      console.log('Test case 4: passed')
+      console.log('Test 3, array of string: passed')
     }
   }
 
-  { // Test case 5
-    const arr5 = ['apple', 'banana', 'orange', 'banana']
+  { // Test case 4: wrong argument
+    const arr = ['apple', 'banana', 'orange']
+    const expected = -1
+    const actual = arr.indexOf('grape')
 
-    if (arr5.indexOf('banana') !== 1) {
-      console.error('Test case 5 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 4, wrong argument: failed, expected: ${expected}, but got actual: ${actual}`)
     } else {
-      console.log('Test case 5: passed')
+      console.log('Test 4, wrong argument: passed')
+    }
+  }
+
+  { // Test case 5: array of string normal case
+    const arr = ['apple', 'banana', 'orange', 'banana']
+    const expected = 1
+    const actual = arr.indexOf('banana')
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test case 5, array of string normal case: failed, expected: ${expected}, but got actual: ${actual}`)
+    } else {
+      console.log('Test 5, array of string normal case: passed')
     }
   }
 }
