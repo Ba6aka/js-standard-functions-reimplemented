193bf30dc3db6aec0cd4807a4c5fe08253714f45
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-17 22:00:01 +0300

    refactor test-join

--- array/instance/join/test-join.js
+++ array/instance/join/test-join.js
@@ -1,59 +1,65 @@
 require('./join.js')
 
 function testJoin() {
+  const { stringify } = JSON
 
-  { // Test case 1
-    const arr1 = ['a', 'b', 'c']
-    const result = arr1.join()
+  { // Test case 1: normal case
+    const arr = ['a', 'b', 'c']
+    const expected = 'a,b,c'
+    const actual = arr.join()
 
-    if (result !== 'a,b,c') {
-      console.error('Test case 1 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 1, normal case: failed, expected: ${expected}, but got actual: ${actual} `)
     } else {
-      console.log('Test case 1: passed')
+      console.log('Test 1, normal case: passed')
     }
   }
 
-  { // Test case 2
-    const arr2 = ['a', 'b', 'c']
-    const result = arr2.join('')
+  { // Test case 2: string without comma
+    const arr = ['a', 'b', 'c']
+    const expected = 'abc'
+    const actual = arr.join('')
 
-    if (result !== 'abc') {
-      console.error('Test case 2 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 2, string without comma: failed, expected: ${expected}, but got actual: ${actual} `)
     } else {
-      console.log('Test case 2: passed')
+      console.log('Test 2, string without comma: passed')
     }
   }
 
-  { //Test case 3
-    const arr3 = ['a', 'b', 'c']
-    const result = arr3.join('-')
+  { //Test case 3: dash argument
+    const arr = ['a', 'b', 'c']
+    const expected = 'a-b-c'
+    const actual = arr.join('-')
 
-    if (result !== 'a-b-c') {
-      console.error('Test case 3 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 3, dash argument: failed, expected: ${expected}, but got actual: ${actual} `)
     } else {
-      console.log('Test case 3: passed')
+      console.log('Test 3, dash argument: passed')
     }
   }
 
   { //Test case 4: empty array
-    const arr4 = []
-    const result = arr4.join()
+    const arr = []
+    const expected = ''
+    const actual = arr.join()
 
-    if (result !== '') {
-      console.error('Test case 4 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 4, empty array: failed, expected: ${expected}, but got actual: ${actual} `)
     } else {
-      console.log('Test case 4: passed')
+      console.log('Test 4, empty array: passed')
     }
   }
 
   { //Test case 5: numbers array
-    const arr5 = [1, 2, 3]
-    const result = arr5.join('|')
+    const arr = [1, 2, 3]
+    const expected = '1|2|3'
+    const actual = arr.join('|')
 
-    if (result !== '1|2|3') {
-      console.error('Test case 5 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 5, numbers array: failed, expected: ${expected}, but got actual: ${actual} `)
     } else {
-      console.log('Test case 5: passed')
+      console.log('Test 5, numbers array: passed')
     }
   }
 }
