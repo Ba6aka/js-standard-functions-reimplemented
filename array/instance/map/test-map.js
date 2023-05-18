c74355c0ef6e6da421ecf16cb4f5961cdea57be1
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-18 20:09:23 +0300

    refactor test-map

--- array/instance/map/test-map.js
+++ array/instance/map/test-map.js
@@ -1,59 +1,65 @@
 require('./map.js')
 
 function testMap() {
+  const { stringify } = JSON
 
-  { // Test case 1
-    const arr1 = [1, 2, 3]
-    const result = arr1.map(x => x * 2)
+  { // Test case 1: normal case
+    const arr = [1, 2, 3]
+    const expected = '2,4,6'
+    const actual = arr.map(x => x * 2)
 
-    if (result.toString() !== '2,4,6') {
-      console.error('Test case 1 failed')
+    if (actual.toString() !== '2,4,6') {
+      console.error(`Test 1, normal case: failed, expected ${expected},but got ${actual}`)
     } else {
-      console.log('Test case 1: passed')
+      console.log('Test 1, normal case: passed')
     }
   }
 
-  { // Test case 2
-    const arr2 = ['apple', 'banana', 'orange']
-    const result = arr2.map(fruit => fruit.toUpperCase())
+  { // Test case 2: to upper case
+    const arr = ['apple', 'banana', 'orange']
+    const expected = 'APPLE,BANANA,ORANGE'
+    const actual = arr.map(fruit => fruit.toUpperCase())
 
-    if (result.toString() !== 'APPLE,BANANA,ORANGE') {
-      console.error('Test case 2 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 2, to upper case: failed, expected ${expected},but got ${actual}`)
     } else {
-      console.log('Test case 2: passed')
+      console.log('Test 2, to upper case: passed')
     }
   }
 
-  { // Test case 3
-    const arr3 = ['1', '2', '3']
-    const result = arr3.map(str => parseInt(str))
+  { // Test case 3: parse integer
+    const arr = ['1', '2', '3']
+    const expected = '1,2,3'
+    const actual = arr.map(str => parseInt(str))
 
-    if (result.toString() !== '1,2,3') {
-      console.error('Test case 3 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 3, parse integer: failed, expected ${expected},but got ${actual}`)
     } else {
-      console.log('Test case 3: passed')
+      console.log('Test 3, parse integer: passed')
     }
   }
 
   { // Test case 4: empty array
-    const arr4 = []
-    const result = arr4.map(x => x * 2)
+    const arr = []
+    const expected = ''
+    const actual = arr.map(x => x * 2)
 
-    if (result.toString() !== '') {
-      console.error('Test case 4 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 4, empty array: failed, expected ${expected},but got ${actual}`)
     } else {
-      console.log('Test case 4: passed')
+      console.log('Test 4, empty array: passed')
     }
   }
 
   { // Test case 5: mixed array
-    const arr5 = ['1', 2, '3']
-    const result = arr5.map(x => parseInt(x))
+    const arr = ['1', 2, '3']
+    const expected = '1,2,3'
+    const actual = arr.map(x => parseInt(x))
 
-    if (result.toString() !== '1,2,3') {
-      console.error('Test case 5 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 5, mixed array: failed, expected ${expected},but got ${actual}`)
     } else {
-      console.log('Test case 5: passed')
+      console.log('Test 5, mixed array: passed')
     }
   }
 }
