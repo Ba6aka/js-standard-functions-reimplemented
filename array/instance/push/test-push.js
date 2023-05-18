4f4beffcb6c0cb604b28faa76ee0e8b467e5fc48
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-18 21:08:11 +0300

    refactir test-push

--- array/instance/push/test-push.js
+++ array/instance/push/test-push.js
@@ -1,53 +1,65 @@
 require('./push.js')
 
 function testPush() {
-  { // Test case 1
-    const arr1 = ['a', 'b', 'c']
-    const newLength = arr1.push('d', 'e')
-    if (newLength !== 5 || arr1[3] !== 'd' || arr1[4] !== 'e') {
-      console.error('Test case 1 failed')
+  const { stringify } = JSON
+
+  { // Test case 1: array of strings
+    const arr = ['a', 'b', 'c']
+    const expected = 5 
+    const actual = arr.push('d', 'e')
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 1, array of strings: failed, expected ${expected},but got ${actual}`)
     } else {
-      console.log('Test case 1: passed')
+      console.log('Test 1, array of strings: passed')
     }
   }
 
-  { // Test case 2
-    const arr2 = [1, 2, 3]
-    const newLength = arr2.push(4)
-    if (newLength !== 4 || arr2[3] !== 4) {
-      console.error('Test case 2 failed')
+  { // Test case 2: array of numbers
+    const arr = [1, 2, 3]
+    const expected = 4
+    const actual = arr.push(4)
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 2, array of numbers: failed, expected ${expected},but got ${actual}`)
     } else {
-      console.log('Test case 2: passed')
+      console.log('Test 2, array of numbers: passed')
     }
   }
 
-  { // Test case 3
-    const arr3 = []
-    const newLength = arr3.push(true, false)
-    if (newLength !== 2 || arr3[0] !== true || arr3[1] !== false) {
-      console.error('Test case 3 failed')
+  { // Test case 3: pushed boolean values to empty array
+    const arr = []
+    const expected = 2  
+    const actual = arr.push(true, false)
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 3, pushed boolean values to empty array: failed, expected ${expected},but got ${actual}`)
     } else {
-      console.log('Test case 3: passed')
+      console.log('Test 3, pushed boolean values to empty array: passed')
     }
   }
 
   { // Test case 4: pushing to empty array
-    const arr4 = []
-    const newLength = arr4.push('a')
-    if (newLength !== 1 || arr4[0] !== 'a') {
-      console.error('Test case 4 failed')
+    const arr = []
+    const expected = 1
+    const actual = arr.push('a')
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 4, pushing to empty array: failed, expected ${expected},but got ${actual}`)
     } else {
-      console.log('Test case 4: passed')
+      console.log('Test 4, pushing to empty array: passed')
     }
   }
 
   { // Test case 5: pushing multiple items to empty array
-    const arr5 = []
-    const newLength = arr5.push(1, 2, 3)
-    if (newLength !== 3 || arr5[0] !== 1 || arr5[1] !== 2 || arr5[2] !== 3) {
-      console.error('Test case 5 failed')
+    const arr = []
+    const expected = 3
+    const actual = arr.push(1, 2, 3)
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 5, pushing multiple items to empty array: failed, expected ${expected},but got ${actual}`)
     } else {
-      console.log('Test case 5: passed')
+      console.log('Test 5, pushing multiple items to empty array: passed')
     }
   }
 }
