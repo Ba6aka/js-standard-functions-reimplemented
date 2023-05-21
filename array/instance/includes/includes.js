6cec3d1b97bd3f6c41788e165c2510d0b6cae136
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-21 19:32:09 +0300

    refactor includes

--- array/instance/includes/includes.js
+++ array/instance/includes/includes.js
@@ -1,16 +1,10 @@
 Array.prototype.includes = function includes(searchElement, fromIndex = 0) {
-  if (fromIndex >= this.length) {
-    return false
-  }
+  if (fromIndex >= this.length) return false
 
-  if (fromIndex < 0) {
-    fromIndex = Math.max(this.length + fromIndex, 0)
-  }
+  if (fromIndex < 0) fromIndex = Math.max(this.length + fromIndex, 0)
 
   for (let i = fromIndex; i < this.length; i++) {
-    if (this[i] === searchElement) {
-      return true
-    }
+    if (this[i] === searchElement) return true
   }
 
   return false
