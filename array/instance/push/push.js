f1ce2acf9e5cf99345635f6c14e7e51497bf68d2
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-21 19:47:51 +0300

    refactor push

--- array/instance/push/push.js
+++ array/instance/push/push.js
@@ -1,7 +1,9 @@
 Array.prototype.push = function(...items) {
   const originalLength = this.length
+
   for (let i = 0; i < items.length; i++) {
     this[originalLength + i] = items[i]
   }
+  
   return this.length
 }
