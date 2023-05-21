f5d40b74fb6c8cda99753bce6e22264bbe65a987
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-21 19:47:42 +0300

    refactor pop

--- array/instance/pop/pop.js
+++ array/instance/pop/pop.js
@@ -2,7 +2,10 @@ Array.prototype.pop = function pop() {
   if (this.length === 0) {
     return undefined
   }
+
   const lastItem = this[this.length - 1]
+
   this.length--
+  
   return lastItem
 }
