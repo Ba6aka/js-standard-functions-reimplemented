549e36add6dd026a18ed3f9fc7677818d513f96b
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-22 17:27:41 +0300

    refactor some

--- array/instance/some/some.js
+++ array/instance/some/some.js
@@ -1,8 +1,7 @@
 Array.prototype.some = function some(callback) {
   for (let i = 0; i < this.length; i++) {
-    if (callback(this[i], i, this)) {
-      return true
-    }
+   if (callback(this[i], i, this)) return true
   }
+
   return false
 }
