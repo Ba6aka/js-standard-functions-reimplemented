0c20851d8534a0a5e8b782edc10aa9d7e31b48dd
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-22 17:22:02 +0300

    refactor slice

--- array/instance/slice/slice.js
+++ array/instance/slice/slice.js
@@ -1,14 +1,12 @@
 Array.prototype.slice = function slice(start, end) {
   const length = this.length
+  const result = []
 
   let relativeStart = start == null ? 0 : start < 0 ? Math.max(length + start, 0) : Math.min(start, length)
   let relativeEnd = end == null || end > length ? length : end < 0 ? Math.max(length + end, 0) : Math.min(end, length)
 
-  const result = []
-
-  while (relativeStart < relativeEnd) {
-    result.push(this[relativeStart])
-    relativeStart++
+  for (let i = relativeStart; i < relativeEnd; i++) {
+    result[result.length] = this[i]
   }
 
   return result
