523ec6eae2a23aa3dfff0d22e1b07adc8bfab597
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-22 21:30:06 +0300

    refactor test-sort

--- array/instance/sort/test-sort.js
+++ array/instance/sort/test-sort.js
@@ -1,48 +1,43 @@
 require('./sort.js')
 
 function testSort() {
+  const { stringify } = JSON
 
-  { // Test case 1
-    const arr1 = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
-    const result = arr1.sort()
+  { // Test case 1: normal case
+    const arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+    const expected = [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
+    const actual = arr.sort()
 
-    if (!arraysEqual(result, [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9])) {
-      console.error('Test case 1 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 1, normal case: failed, expected ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 1: passed')
+      console.log('Test 1, normal case: passed')
     }
   }
 
-  { // Test case 2
-    const arr2 = ['banana', 'apple', 'orange', 'pear']
-    const result = arr2.sort()
+  { // Test case 2:  string array
+    const arr = ['banana', 'apple', 'orange', 'pear']
+    const expected = ['apple', 'banana', 'orange', 'pear']
+    const actual = arr.sort()
 
-    if (!arraysEqual(result, ['apple', 'banana', 'orange', 'pear'])) {
-      console.error('Test case 2 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 2, string array: failed, expected ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 2: passed')
+      console.log('Test 2, string array: passed')
     }
   }
 
   { //Test case 3: empty array
-    const arr3 = []
-    const result = arr3.sort()
+    const arr = []
+    const expected = []
+    const actual = arr.sort()
 
-    if (!arraysEqual(result, [])) {
-      console.error('Test case 3 failed')
+    if (stringify(expected) !== stringify(actual)) {
+      console.error(`Test 3, empty array: failed, epxpected ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 3: passed')
+      console.log('Test 3, empty array: passed')
     }
   }
-
-}
-
-function arraysEqual(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false
-  }
-  return true
 }
 
 testSort()
