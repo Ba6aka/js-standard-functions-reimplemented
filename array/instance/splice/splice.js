e261ff9abfea6bc42cff2b9b7259b9d5d94573e6
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-23 17:13:44 +0300

    refactor splice and test-splice

--- array/instance/splice/splice.js
+++ array/instance/splice/splice.js
@@ -5,26 +5,22 @@ Array.prototype.splice = function splice(start, deleteCount, ...items) {
   let count = deleteCount
   let i, j
 
-  // Determine start index
   if (start < 0) {
     i = len + start
   } else {
     i = start
   }
 
-  // Determine delete count
   if (count === undefined) {
     count = len - i
   }
 
-  // Clamp delete count
   if (count > len - i) {
     count = len - i
   }
 
-  // Delete items
   for (j = 0; j < count; j++) {
-    deleted.push(this[i + j])
+    deleted[deleted.length] = (this[i + j])
   }
   
   for (; j < len - i; j++) {
@@ -32,7 +28,6 @@ Array.prototype.splice = function splice(start, deleteCount, ...items) {
   }
   this.length = len - count
 
-  // Insert items
   const itemCount = items.length
   for (j = len - 1; j >= i; j--) {
     this[j + itemCount] = this[j]
