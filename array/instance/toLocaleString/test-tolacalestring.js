e2dec83d6cf8abd2213186df4f5ca0f0ea301b76
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-24 16:32:09 +0300

    refactor test-tolacalestring

--- array/instance/toLocaleString/test-tolacalestring.js
+++ array/instance/toLocaleString/test-tolacalestring.js
@@ -1,49 +1,53 @@
 require('./toLocaleString.js')
 
 function testToLocaleString() {
-  { // Test case 1
-    const arr1 = [1, 2, 3]
-    const result = arr1.toLocaleString()
+  const { stringify } = JSON
 
-    if (result !== '1,2,3') {
-      console.error('Test case 1 failed')
+  { // Test case 1: array of numbers
+    const arr = [1, 2, 3]
+    const expected = '1,2,3'
+    const actual = arr.toLocaleString()
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 1, array of numbers failed: expected ${stringify(expected)}, but got ${stringify(actual)}`)
     } else {
-      console.log('Test case 1: passed')
+      console.log('Test 1, array of numbers: passed')
     }
   }
 
+  { // Test case 2: array of srings
+    const arr = ['one', 'two', 'three']
+    const expected = 'one,two,three'
+    const actual = arr.toLocaleString()
 
-
-  { // Test case 2
-    const arr2 = ['one', 'two', 'three']
-    const result = arr2.toLocaleString()
-
-    if (result !== 'one,two,three') {
-      console.error('Test case 2 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 2, array of strings failed: expected ${stringify(expected)}, but got ${stringify(actual)}`)
     } else {
-      console.log('Test case 2: passed')
+      console.log('Test 2, array of strings: passed')
     }
   }
 
   { // Test case 3: empty array
-    const arr3 = []
-    const result = arr3.toLocaleString()
+    const arr = []
+    const expected = ''
+    const actual = arr.toLocaleString()
 
-    if (result !== '') {
-      console.error('Test case 3 failed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 3, empty array failed: expected ${stringify(expected)}, but got ${stringify(actual)}`)
     } else {
-      console.log('Test case 3: passed')
+      console.log('Test 3, empty array: passed')
     }
   }
 
   { // Test case 4: array with null values
-    const arr4 = [1, null, 'two', undefined, 3]
-    const result = arr4.toLocaleString()
+    const arr = [1, null, 'two', undefined, 3]
+    const expected = '1,,two,,3'
+    const actual = arr.toLocaleString()
 
-    if (result !== '1,,two,,3') {
-      console.error('Test case 4 failed')
-    } else {
-      console.log('Test case 4: passed')
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 4, array with null values failed: expected ${stringify(expected)}, but got ${stringify(actual)}`)
+    } else{
+      console.log('Test 4,ararray with null values : passed')
     }
   }
 }
