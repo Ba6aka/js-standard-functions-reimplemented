1904a2bb0144bdf06a1d1058a19e6c1f9cb88c63
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-24 21:11:17 +0300

    refactor at

--- string/instance/at/at.js
+++ string/instance/at/at.js
@@ -1,23 +1,5 @@
 String.prototype.at = function at(index) {
-  const str = this.toString()
-  const length = str.length >>> 0
-
-  if (index < 0) {
-    return str.charAt(length + index)
-  }
-
-  if (index >= length) {
-    return undefined
-  }
-
-  const charCode = str.charCodeAt(index)
-  const first = charCode & 0xfc00
-
-  if (first === 0xd800) {
-    const second = str.charCodeAt(index + 1) & 0x3ff
-
-    return String.fromCharCode(((charCode & 0x3ff) << 10) + second + 0x10000)
-  } else {
-    return str.charAt(index)
-  }
+  index |= 0
+  
+  return index < 0 ? this[this.length + index] : this[index]
 }
