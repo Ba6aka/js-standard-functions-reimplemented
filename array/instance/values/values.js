75fca741fbe54337e52b357c0f9839d942fe3973
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-24 17:01:34 +0300

    refactor values

--- array/instance/values/values.js
+++ array/instance/values/values.js
@@ -2,6 +2,7 @@ Array.prototype.values = function values() {
   let i = 0
   const array = this
   return {
+    [Symbol.iterator]:values.bind(this),
     next: function() {
       if (i < array.length) {
         return { value: array[i++], done: false }
