20024077028ba41c7a5d752aee4b4886b319fe00
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-27 17:25:45 +0300

    refactor test-includes

--- string/instance/includes/test-includes.js
+++ string/instance/includes/test-includes.js
@@ -1,27 +1,29 @@
 require('./includes.js')
 
-function testIncludes() {
-  {// Test case 1: standard
-    const str = "Hello, world!"
-    const result = str.includes("world")
+testIncludes()
 
-    if (result !== true) {
-      console.error('Test case 1 failed')
+function testIncludes() {
+  { // Test case 1: normal case
+    const str = "Hello, world!"
+    const expected = true
+    const actual = str.includes("world")
+
+    if (actual !== expected) {
+      console.error(`Test 1, normal case: failed, expected: ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 1 passed')
+      console.log('Test 1, normal case: passed')
     }
   }
 
-  {// Test case 2: with start position
+  { // Test case 2: with start position
     const str = "Hello, world!"
-    const result = str.includes("world", 7)
+    const expected = true
+    const actual = str.includes("world", 7)
 
-    if (result !== true) {
-      console.error('Test case 2 failed')
+    if (actual !== expected) {
+      console.error('Test 2, with start position: failed')
     } else {
-      console.log('Test case 2 passed')
+      console.log('Test 2, with start position: passed')
     }
   }
 }
-
-testIncludes()
