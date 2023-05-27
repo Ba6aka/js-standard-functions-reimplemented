14fbbe701fd10af4e6da3cdec50e47fb9ea753d6
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-28 01:08:31 +0300

    refactor localecompare

--- string/instance/localeCompare/test-localecompare.js
+++ string/instance/localeCompare/test-localecompare.js
@@ -1,37 +1,46 @@
 require('./localeCompare.js')
 
+testLocaleCompare()
+
 function testLocaleCompare() {
-  {// Test case 1
+  { // Test case 1: normal case
     const str1 = "abc"
     const str2 = "def"
-    if (str1.localeCompare(str2) < 0) {
-      console.log('Test case 1 passed')
+    const actual = str1.localeCompare(str2)
+    const expected = -3
+
+    if (actual !== expected) {
+      console.error(`Test 1, normal case: failed, expected: ${expected}, but got ${actual}`)
     } else {
-      console.error('Test case 1 failed')
+      console.log('Test 1, normal case: passed')
     }
   }
 
-{// Test case 2
-  const str3 = "xyz"
-  const str4 = "abc"
-  if (str3.localeCompare(str4) <= 0) {
-    console.error('Test case 2 failed')
+{ // Test case 2: more than 0
+  const str1 = "xyz"
+  const str2 = "abc"
+  const expected = 23
+  const actual = str1.localeCompare(str2)
+
+  if (actual !== expected) {
+    console.error(`Test 2, more than 0: failed, expected: ${expected}, but got ${actual}`)
   } else {
-    console.log('Test case 2 passed')
+    console.log('Test 2, more than 0: passed')
   }
 }
 
-  {// Test case 3
-    const str5 = "foo"
-    const str6 = "foo"
-    if (str5.localeCompare(str6) !== 0) {
-      console.error('Test case 3 failed')
+  { // Test case 3: equal 0
+    const str1 = "foo"
+    const str2 = "foo"
+    const expected = 0
+    const actual = str1.localeCompare(str2)
+
+    if (actual !== expected) {
+      console.error(`Test 3, equal 0: failed, expected: ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 3 passed')
+      console.log('Test 3, equal 0: passed')
     }
   }
 
 
 }
-
-testLocaleCompare()
