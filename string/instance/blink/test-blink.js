a3b7b1abf4290093440121aa9046119d4625b0b0
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-27 15:30:39 +0300

    refactor testBlink

--- string/instance/blink/test-blink.js
+++ string/instance/blink/test-blink.js
@@ -3,7 +3,7 @@ require('./blink.js')
 testBlink()
 
 function testBlink() {
-  {// Test case 1: normal case 
+  { // Test case 1: normal case 
     const str = 'hello'
     const expected = '<blink>hello</blink>'
     const actual = str.blink()
@@ -15,14 +15,15 @@ function testBlink() {
     }
   }
 
-  {// Test case 2: empty string
+  { // Test case 2: empty string
     const str = ' '
+    const expected = '<blink> </blink>'
     const actual = str.blink()
 
     if (actual !== expected) {
-      console.error('Test case 2 failed')
+      console.error(`Test 2, empty string: failed, expected: ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 2 passed')
+      console.log('Test 2, empty string: passed')
     }
   }
 }
