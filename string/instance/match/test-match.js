9193818f9414093cfc6d1d3e1da2e63850d526a2
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-28 01:24:29 +0300

    refactor test-match

--- string/instance/match/test-match.js
+++ string/instance/match/test-match.js
@@ -1,42 +1,46 @@
 require('./match.js')
 
-function testMatch() {
-  {// Test case 1
-    const str1 = "The quick brown fox jumps over the lazy dog."
-    const regexp1 = /o/g
-    const result1 = str1.match(regexp1)
-
-    if (result1.toString() !== ['o', 'o', 'o', 'o'].toString()) {
-      console.error('Test case 1 failed')
-    } else {
-      console.log('Test case 1 passed')
-    }
-  }
-
-  {// Test case 2
-    const str2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    const regexp2 = /e/g
-    const result2 = str2.match(regexp2)
-
-    if (result2.toString() !== ['e', 'e', 'e', 'e', 'e'].toString()) {
-      console.error('Test case 2 failed')
-    } else {
-      console.log('Test case 2 passed')
-    }
-  }
-
-  {// Test case 3
-    const str3 = "The quick brown fox jumps over the lazy dog."
-    const regexp3 = /x/g
-    const result3 = str3.match(regexp3)
-
-    if (result3.toString() !== ['x'].toString()) {
-      console.error('Test case 3 failed')
-    } else {
-      console.log('Test case 3 passed')
-    }
-  }
-  
-}
-
 testMatch()
+
+function testMatch() {
+  const { stringify } = JSON
+
+  { // Test case 1: match o
+    const str = "The quick brown fox jumps over the lazy dog."
+    const regexp = /o/g
+    const expected = ['o', 'o', 'o', 'o']
+    const actual = str.match(regexp)
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 1, match o: failed, expected: ${stringify(expected)}, but got : ${stringify(actual)}`)
+    } else {
+      console.log('Test 1, match o: passed')
+    }
+  }
+
+  { // Test case 2: match e
+    const str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    const regexp = /e/g
+    const expected = ['e', 'e', 'e', 'e', 'e']
+    const actual = str.match(regexp)
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 2, match e: failed, expected: ${stringify(expected)}, but got : ${stringify(actual)}`)
+    } else {
+      console.log('Test 2, match e: passed')
+    }
+  }
+
+  { // Test case 3: match x
+    const str = "The quick brown fox jumps over the lazy dog."
+    const regexp = /x/g
+    const expected = ['x']
+    const actual = str.match(regexp)
+
+    if (stringify(actual) !== stringify(expected)) {
+      console.error(`Test 3, match x: failed, expected: ${stringify(expected)}, but got : ${stringify(actual)}`)
+    } else {
+      console.log('Test 3, match x: passed')
+    }
+  }
+}
