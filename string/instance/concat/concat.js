4d8a04a039be5117b2ff8de981e7e3d30a5640ba
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-27 17:01:13 +0300

    refactor test-concat

--- string/instance/concat/concat.js
+++ string/instance/concat/concat.js
@@ -1,17 +1,8 @@
-String.prototype.concat = function concat(...strings) {
-  let result = ''
+String.prototype.concat = function (...strings) {
+  let result = this
 
-  // Add the current string to the result
-  for (let i = 0; i < this.length; i++) {
-    result += this[i]
-  }
-
-  // Add each additional string to the result
-  for (let i = 0; i < strings.length; i++) {
-    const str = strings[i]
-    for (let j = 0; j < str.length; j++) {
-      result += str[j]
-    }
+  for (const str of strings) {
+    result += str
   }
 
   return result
