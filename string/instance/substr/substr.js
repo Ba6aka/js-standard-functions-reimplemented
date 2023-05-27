e09dfdb60082164d853530f6c291b65b7c59ff94
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-28 01:33:32 +0300

    refator substr

--- string/instance/substr/substr.js
+++ string/instance/substr/substr.js
@@ -1,20 +1,12 @@
-String.prototype.substr = function substr(start, length) {
-  let result = ''
-  let str = this
-  const strLength = str.length
-
-  // Normalize negative start value
-  if (start < 0) {
-    start = Math.max(strLength + start, 0)
-  }
-
-  // Calculate end index
+String.prototype.substr = function (start, length) {
+  const strLength = this.length
+  start = start < 0 ? Math.max(strLength + start, 0) : start
   const end = length !== undefined ? Math.min(start + length, strLength) : strLength
-
-  // Build the resulting string
+  
+  let result = ''
+  
   for (let i = start; i < end; i++) {
-    result += str[i]
+    result += this[i]
   }
-
   return result
 }
