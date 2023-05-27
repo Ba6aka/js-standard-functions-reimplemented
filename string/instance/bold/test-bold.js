cc288254afe3cc8432420616646f8bd557786bb6
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-27 15:32:41 +0300

    refactor testBold

--- string/instance/bold/test-bold.js
+++ string/instance/bold/test-bold.js
@@ -1,27 +1,29 @@
 require('./bold.js')
 
-function testBold() {
-  {  // Test case 1: standard
-    const str = "hello"
-    const boldStr = str.bold()
+testBold()
 
-    if (boldStr !== "<b>hello</b>") {
-      console.error("Test case 1 failed")
+function testBold() {
+  { // Test case 1: normal case
+    const str = "hello"
+    const expected = "<b>hello</b>"
+    const actual = str.bold()
+
+    if (actual !== expected) {
+      console.error(`Test 1, normal case: failed, expected: ${expected}, but got ${actual}`)
     } else {
-      console.log("Test case 1 passed")
+      console.log(`Test 1, normal case: passed`)
     }
   }
 
-  {// Test case 2: empty string
+  { // Test case 2: empty string
     const str = ""
-    const boldStr = str.bold()
-    
-    if (boldStr !== "<b></b>") {
-      console.error("Test case 2 failed")
+    const expected = "<b></b>"
+    const actual = str.bold()
+
+    if (actual !== expected) {
+      console.error(`Test 2, empty string: failed, expected: ${expected}, but got ${actual}`)
     } else {
-      console.log("Test case 2 passed")
+      console.log('Test 2, empty string: passed')
     }
   }
 }
-
-testBold()
