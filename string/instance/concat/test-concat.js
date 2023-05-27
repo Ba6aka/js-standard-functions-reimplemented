9cebc0e38cb36a9edae1d4ee7ed043c6bead63b8
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-27 17:04:34 +0300

    refactor test-concat

--- string/instance/concat/test-concat.js
+++ string/instance/concat/test-concat.js
@@ -1,43 +1,47 @@
 require('./concat.js')
 
+testConcat()
+
 function testConcat() {
-  {// Test case 1: standard
+  { // Test case 1: normal case
     const str1 = "Hello, "
     const str2 = "world!"
-    const str3 = str1.concat(str2)
+    const expected = "Hello, world!"
+    const actual = str1.concat(str2)
 
-    if (str3 !== "Hello, world!") {
-      console.error('Test case 1 failed')
+    if (actual !== expected) {
+      console.error(`Test 1, normal case: failed, expected: ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 1 passed')
+      console.log('Test 1, normal case: passed')
     }
   }
 
-  {// Test case 2: three strings
+  { // Test case 2: three strings
     const str1 = "foo"
     const str2 = "bar"
     const str3 = "baz"
-    const str4 = str1.concat(str2, str3)
+    const expected = "foobarbaz"
+    const actual = str1.concat(str2, str3)
 
-    if (str4 !== "foobarbaz") {
-      console.error('Test case 2 failed')
+    if (actual !== expected) {
+      console.error(`Test 2, three strings: failed, expected: ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 2 passed')
+      console.log('Test 2, three strings: passed')
     }
   }
 
-  {// Test case 3: empty string
+  { // Test case 3: empty string
     const str1 = ""
     const str2 = "Hello, world!"
-    const str3 = str1.concat(str2)
+    const expected = "Hello, world!"
+    const actual = str1.concat(str2)
 
-    if (str3 !== "Hello, world!") {
-      console.error('Test case 3 failed')
+    if (actual !== expected) {
+      console.error(`Test 3, empty string: failed, expected: ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 3 passed')
+      console.log('Test 3, empty string: passed')
     }
   }
 
 }
 
-testConcat()
