e7c786e3652522719d486f546a246c2916a9229d
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-28 00:58:32 +0300

    refactor testLink

--- string/instance/link/test-link.js
+++ string/instance/link/test-link.js
@@ -1,29 +1,31 @@
 require('./link.js')
 
+testLink()
+
 function testLink() {
-  {// Test case 1
+  { // Test case 1
     const str = "Google"
     const url = "https://www.google.com"
-    const link = str.link(url)
+    const expected = '<a href="https://www.google.com">Google</a>'
+    const actual = str.link(url)
 
-    if (link !== '<a href="https://www.google.com">Google</a>') {
-      console.error('Test case 1 failed')
+    if (actual !== expected) {
+      console.error(`Test 1, normal case: failed, expected: ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 1 passed')
+      console.log('Test 1, normal case: passed')
     }
   }
 
-  {// Test case 2: empty string
+  { // Test case 2: empty string
     const str = ""
     const url = "https://www.example.com"
-    const link = str.link(url)
+    const expected = '<a href="https://www.example.com"></a>'
+    const actual = str.link(url)
 
-    if (link !== '<a href="https://www.example.com"></a>') {
-      console.error('Test case 2 failed')
+    if (actual !== expected) {
+      console.error(`Test 2, empty string: failed, expected: ${expected}, but got ${actual}`)
     } else {
-      console.log('Test case 2 passed')
+      console.log('Test 2, empty string: passed')
     }
   }
 }
-
-testLink()
