ba6674193291afeedb7dc213fedb7cc677d9ebb1
Ba6aka <bam6ykba6aka@gmail.com>
2023-05-31 21:37:53 +0300

    refactor test-matchall

--- string/instance/matchAll/test-matchall.js
+++ string/instance/matchAll/test-matchall.js
@@ -1,52 +1,52 @@
 require('./matchAll.js')
 
+testMatchAll()
+
 function testMatchAll() {
-    {// Test case 1
-      const str1 = "Hello, world!"
-      const regex1 = /\w+/g
-      const matches1 = str1.matchAll(regex1)
-      const expected1 = [["Hello", 0], ["world", 7]]
-  
-      for (let i = 0; i < matches1.length; i++) {
-        const match = matches1[i]
-        
-        if (match[0] !== expected1[i][0] || match.index !== expected1[i][1]) {
-          console.error('Test case 1 failed')
-          return
-        }
-      }
-  
-      console.log('Test case 1 passed')
-    }
+  { // Test case 1: two arrays
+    const str = "Hello, world!"
+    const regex = /\w+/g
+    const matches = str.matchAll(regex)
+    const expected = [["Hello", 0], ["world", 7]]
 
-    
-  {// Test case 2
-    const str2 = "The quick brown fox jumps over the lazy dog."
-    const regex2 = /\w+/g
-    const matches2 = str2.matchAll(regex2)
-    const expected2 = [      ["The", 0],
-      ["quick", 4],
-      ["brown", 10],
-      ["fox", 16],
-      ["jumps", 20],
-      ["over", 26],
-      ["the", 31],
-      ["lazy", 35],
-      ["dog", 40]
-    ]
+    for (let i = 0; i < matches.length; i++) {
+      const actual = matches[i]
 
-    for (let i = 0; i < matches2.length; i++) {
-      const match = matches2[i]
-      
-      if (match[0] !== expected2[i][0] || match.index !== expected2[i][1]) {
-        console.error('Test case 2 failed')
+      if (actual[0] !== expected[i][0] || actual.index !== expected[i][1]) {
+        console.error(`Test 1, two arrays: failed, expexted word: ${expected[i][0]}, but got ${actual[0]}, expected index: ${expected[i][1]}, but got ${actual.index}`)
         return
       }
     }
 
-    console.log('Test case 2 passed')
+    console.log('Test 1, two arrays: passed')
+  }
+
+
+  { // Test case 2: a lot of arrays
+    const str = "The quick brown fox jumps over the lazy dog."
+    const regex = /\w+/g
+    const matches = str.matchAll(regex)
+    const expected = [["The", 0],
+    ["quick", 4],
+    ["brown", 10],
+    ["fox", 16],
+    ["jumps", 20],
+    ["over", 26],
+    ["the", 31],
+    ["lazy", 35],
+    ["dog", 40]
+    ]
+
+    for (let i = 0; i < matches.length; i++) {
+      const match = matches[i]
+
+      if (match[0] !== expected[i][0] || match.index !== expected[i][1]) {
+        console.error(`Test 2, a lot of arrays: failed, expexted word: ${expected[i][0]}, but got ${actual[0]}, expected index: ${expected[i][1]}, but got ${actual.index}`)
+        return
+      }
+    }
+
+    console.log('Test 2, a lot of arrays: passed')
   }
 
 }
-
-testMatchAll()
